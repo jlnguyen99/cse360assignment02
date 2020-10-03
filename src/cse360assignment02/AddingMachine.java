156a4d1512600b22ad2556df5dcee501d44e3ba1
jlnguyen99 <68876229+jlnguyen99@users.noreply.github.com>
2020-10-03 13:05:57 -0700

    Added JavaDoc comments

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -8,24 +8,43 @@ public class AddingMachine {
   private List<Integer> history = new ArrayList<Integer>();
   private String string = "0";
   
+  /**
+  * Constructor of the object AddingMachine
+  */  
   public AddingMachine () {
 	  total = 0;  // not needed - included for clarity
   }
   
+  /**
+  * Returns the total
+  * @return total the total after all the calculations
+  */
   public int getTotal () {
 	  return total;
   }
   
+  /**
+  * Adds the value given to the total
+  * @param value the number that needs to be added to the total
+  */
   public void add (int value) {
 	  total = total + value;
 	  history.add (value);
   }
 
+  /**
+  * Subtracts the value given to the total
+  * @param value the number that needs to be subtracted from the total
+  */
   public void subtract (int value) {
 	  total = total - value;
 	  history.add (-1 * value);
   }
 
+  /**
+  * Overrides the toString method for System.out.println
+  * @return (name of what gets returned *and what it is)
+  */
   public String toString () {
 	  for (int i = 0; i < history.size (); i++) {
 		  if (history.get(i) < 0) {
@@ -38,9 +57,15 @@ public class AddingMachine {
 	  return string;
   }
 
+  /**
+  * Method that isn't used/defined in assignment
+  */
   public void clear () {
   }
   
+  /**
+  * Main method to implement the object AddingMachine and its functions
+  */
   public static void main(String[] args) {
 	  AddingMachine adder = new AddingMachine();
 	  
